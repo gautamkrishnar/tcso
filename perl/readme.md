8d8740f4b3ef2278cf8a145c8199afe739983854
Gautam krishna.R <r.gautamkrishna@gmail.com>
2016-05-02 12:00:56 +0530

    added perl tcso

---
+++ perl/readme.md
@@ -0,0 +1,9 @@
+# TcSo PHP
+This is the PHP version of the Try catch stack overflow. Use the try catch block used in [tcso.php](tcso.php). You can use it in your projects. Please don't forget to make  improvements and submit a new pull request.
+#### Requirements
+Uses perl trycatch module. Remember to include it in your code by using  `use TryCatch;` statement.
+#### Credits
+Developed by: [Gautam krishna R](https://github.com/gautamkrishnar/)
+
+###### Contributors
+* {your-name-here}
---
+++ perl/tcso.pl
@@ -0,0 +1,19 @@
+#Uses perl TryCatch module.
+ use TryCatch;
+
+try {
+     # try something wrong here
+}
+catch (e) {
+    my $url ="http://stackoverflow.com/search?q=[js]+".e.getMessage();
+    my $platform = $^O;
+    my $cmd;
+    if    ($platform eq 'darwin')  { $cmd = "open \"$url\"";          } # Mac OS X
+    elsif ($platform eq 'linux')   { $cmd = "x-www-browser \"$url\""; } # Linux
+    elsif ($platform eq 'MSWin32') { $cmd = "start $url";             } # Win95..Win10
+    if (defined $cmd) {
+        system($cmd);
+    } else {
+        die "Can't locate default browser";
+    }
+}
