1db9a3cd40925f85fb0a74ba55934c3b30534f79
Adam Chainz <me@adamj.eu>
2016-07-06 16:39:44 +0100

    Tidy python implementation
    1. Make it Python 3 compatible (exceptions no longer have `.message` on Python 3).
    2. Make it pass the popular `pycodestyle` ("PEP 8") format
    3. Comment on what `new=2` means

--- python/tcso.py
+++ python/tcso.py
@@ -1,7 +1,8 @@
 import webbrowser
-new = 2
+
+
 try:
-    /# Do somethomh wrong here
+    pass  # Do something wrong here
 except Exception as e:
-    url = "http://stackoverflow.com/search?q=[python]+"+e.message;
-    webbrowser.open(url,new=new);
+    url = "http://stackoverflow.com/search?q=[python]+" + str(e)
+    webbrowser.open(url, new=2)  # new=2 means new tab
