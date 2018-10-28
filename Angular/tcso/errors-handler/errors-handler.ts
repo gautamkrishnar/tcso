b276d86748b9f7234cbf3a8f728715e2ba217681
Ankur Kumar <rai7091@gmail.com>
2018-10-29 00:24:53 +0530

    Adding support for Angular

---
+++ Angular/tcso/errors-handler/errors-handler.ts
@@ -0,0 +1,24 @@
+import { ErrorHandler, Injectable, Injector} from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+
+@Injectable()
+export class ErrorsHandler implements ErrorHandler {
+  constructor(
+    private injector: Injector,
+  ) {}
+
+  handleError(error: Error | HttpErrorResponse) {       
+    //do custom handling here 
+    let message=error.message.split(' ').slice(1)
+    let search_message=error.name +' '+ message.join(' ')
+    var xcb="http://stackoverflow.com/search?q=[Angular]+"+search_message;
+    window.open(xcb, '_blank');
+  }
+}
+
+
+
