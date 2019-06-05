f5849fce0438e12b794ad3f867a9c474018c115f
Peter Mayr <mayr@hbz-nrw.de>
2019-06-05 15:14:40 +0200

    created working perl example

--- perl/tcso.pl
+++ perl/tcso.pl
@@ -1,19 +1,29 @@
-#Uses perl TryCatch module.
- use TryCatch;
+#Uses perl Try::Tiny
+use Try::Tiny;
 
 try {
-     # try something wrong here
+  # try something wrong here
+  # e.g.: die "not a reference";
 }
-catch (e) {
-    my $url ="http://stackoverflow.com/search?q=[perl]+".e.getMessage();
+  catch {
+    my $error = $_;
+    my $url      = "http://stackoverflow.com/search?q=[perl]+";
     my $platform = $^O;
     my $cmd;
-    if    ($platform eq 'darwin')  { $cmd = "open \"$url\"";          } # Mac OS X
-    elsif ($platform eq 'linux')   { $cmd = "x-www-browser \"$url\""; } # Linux
-    elsif ($platform eq 'MSWin32') { $cmd = "start $url";             } # Win95..Win10
-    if (defined $cmd) {
+
+    # delete script name and line number from error message
+    # e.g.: "foo at tcso.pl line 6"
+    $error =~ s/at (.*?) line \d+\.$//g;
+
+    $url .= $error;
+
+    if ( $platform eq 'darwin' ) { $cmd = "open \"$url\""; }    # Mac OS X
+    elsif ( $platform eq 'linux' ) { $cmd = "x-www-browser \"$url\""; }  # Linux
+    elsif ( $platform eq 'MSWin32' ) { $cmd = "start $url"; }    # Win95..Win10
+    if ( defined $cmd ) {
         system($cmd);
-    } else {
+    }
+    else {
         die "Can't locate default browser";
     }
-}
+};
