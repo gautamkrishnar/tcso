1ea8aae05d361d94be64be946d0566968327eabb
Valdas <valdas771@gmail.com>
2019-10-14 21:32:32 +0300

    Make c# example platform independent

--- C#/tcso.cs
+++ C#/tcso.cs
@@ -1,8 +1,40 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 try
 {
     // Try something wrong here 
 }
 catch (System.Exception ex)
 {
-    System.Diagnostics.Process.Start("http://stackoverflow.com/search?q=[c%23]+" + ex.Message);
+	var url = @"http://stackoverflow.com/search?q=[c%23]+" + ex.Message.Replace(" ", "%20");
+	OpenUrl(url);
+}
+
+static void OpenUrl(string url)
+{
+	try
+	{
+		Process.Start(url);
+	}
+	catch
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			url = url.Replace("&", "^&");
+			Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+		}
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+		{
+			Process.Start("xdg-open", url);
+		}
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			Process.Start("open", url);
+		}
+		else
+		{
+			throw;
+		}
+	}
 }
